d24143924fbd2629c8c5f16935fb299e014ae573
cherylfong <42821602+cherylfong@users.noreply.github.com>
2020-09-06 00:09:25 -0700

    add test case

--- Sudoku.java
+++ Sudoku.java
@@ -36,6 +36,7 @@ class Sudoku{
 
     }
 
+    // fill up a 2D array using input from the user
     public int[][] populate2DArray(int row, int col){
 
         int[][] array = new int[row][col];
@@ -210,7 +211,7 @@ class Sudoku{
         if(solution){
             System.out.println("Board Solved!");
         }else{
-            System.out.println("Board unsolvable ='(");
+            System.out.println("Board unsolvable :'(");
         }
     }
 
@@ -251,6 +252,21 @@ class Sudoku{
             { 0, 1, 6, 0, 2, 0, 4, 0, 0 },
             { 0, 9, 0, 5, 8, 0, 7, 0, 0 }
             };
+
+        // unsolvable
+        int[][] test22 = {
+
+        {7, 8, 1, 5, 4, 3, 9, 2, 6 },
+        {0, 0, 6, 1, 7, 9, 5, 0, 0 },
+        {9, 5, 4, 6, 2, 8, 7, 3, 1 },
+        {6, 9, 5, 8, 3, 7, 2, 1, 4 },
+        {1, 4, 8, 2, 6, 5, 3, 7, 9 },
+        {3, 2, 7, 9, 1, 4, 8, 0, 0 },
+        {4, 1, 3, 7, 5, 2, 6, 9, 8 },
+        {0, 0, 2, 0, 0, 0, 4, 0, 0 },
+        {5, 7, 9, 4, 8, 6, 1, 0, 3 },
+
+        };
 
         int[][] test3 = new int[1][2];
         int[][] test6 = new int[25][25];
